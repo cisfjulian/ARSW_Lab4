1afa916a11f74aec82901cfc783c09e76ad3ca1a
cisfjulian <cisfjulian@gmail.com>
2023-02-16 09:02:43 -0500

    taller net realizado

--- Taller NetBeans/src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
+++ Taller NetBeans/src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -2,7 +2,10 @@ package edu.eci.arsw.springdemo;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class SpanishSpellChecker implements SpellChecker {
 
 	@Override
